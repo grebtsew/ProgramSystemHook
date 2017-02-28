ab83da17a448a287a8e0ac26b378d319380c81ba
GREBTSEW <danwe980@student.liu.se>
2017-02-28 03:36:55 +0100

    v0.1
    fixed a couple things, now hook mouse, keyboard, windows, finds
    programs, still need some work with finding programs on start and exit.
    Also need functionallity to save down the data to logfiles for my future
    ai project.

--- ProgramUsage/ProgramUsage/Form1.Designer.cs
+++ ProgramUsage/ProgramUsage/Form1.Designer.cs
@@ -28,12 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(358, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "HookKeyboard On/Off";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 114);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(937, 598);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(966, 718);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Keyboard and mouse";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(407, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(365, 83);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "HookMouse On/Off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(984, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1764, 718);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Program Listener";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(585, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 83);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "List Service";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(16, 114);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(1716, 598);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(235, 83);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "ListActivePrograms";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1160, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(235, 83);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ListenToWindows";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(296, 25);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(235, 83);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "List Active Service";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(856, 25);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(235, 83);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "ListenToPrograms";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(2805, 752);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WindowsListener";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- ProgramUsage/ProgramUsage/Form1.cs
+++ ProgramUsage/ProgramUsage/Form1.cs
@@ -2,19 +2,370 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Windows;
+using System.Threading;
+using System.Management;
+using System.Management.Instrumentation;
 
 namespace ProgramUsage
 {
     public partial class Form1 : Form
     {
+        GlobalKeyboardHook gHook;
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType,
+        IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr
+           hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+           uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        const uint EVENT_OBJECT_NAMECHANGE = 0x800C;
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+
+
+        // Need to ensure delegate is not collected while we're using it,
+        // storing it in a class field is simplest way to do this.
+        static WinEventDelegate procDelegate = new WinEventDelegate(WinEventProc);
+
+        Boolean shift = false;
+        Boolean first2 = true;
+        Boolean mouse = false;
+        Boolean windows = false;
+        Boolean keyboard = false;
+        Boolean first = true;
+        Boolean first3 = true;
+        Boolean programs = false;
+        IntPtr hhook;
+
         public Form1()
         {
+     
             InitializeComponent();
+
+           
+        }
+
+
+       
+ 
+
+    
+      
+    private void Form1_Load_1(object sender, EventArgs e)
+    {
+            
+
+            
+
+    
+        gHook = new GlobalKeyboardHook(); // Create a new GlobalKeyboardHook
+                                              // Declare a KeyDown Event
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+
+            // Add the keys you want to hook to the HookedKeys list
+  
+
+            foreach (Keys key in Enum.GetValues(typeof(Keys))) { 
+        
+            gHook.HookedKeys.Add(key);
+        }
+        }
+
+      
+    
+
+    // Handle the KeyDown Event
+    public void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            // keyboard
+         appendText("Keypress identified :\t" + e.KeyCode + " " + "\t Key number : " + e.KeyValue);
+
+           
+        }
+
+
+    
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+         
+            richTextBox1.ScrollToCaret();
+        }
+
+
+        private void appendText( String s)
+        {
+            richTextBox1.AppendText(s+ "\t Time : " + string.Format("{0:HH:mm:ss tt}", DateTime.Now) + "\t" + DateTime.Today.ToString("d") + "\n");
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (keyboard)
+            {
+                keyboard = false;
+                gHook.unhook();
+                button1.BackColor = Color.Red; 
+            } else
+            {
+                keyboard = true;
+                gHook.hook();
+                button1.BackColor = Color.Green;
+            }
+            
+            
+        }
+
+     
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (mouse)
+            {
+                MouseHook.stop();
+                mouse = false;
+                button3.BackColor = Color.Red;
+            } else
+            {
+                mouse = true;
+
+                MouseHook.Start();
+                if (first) {
+                    first = false;
+                MouseHook.MouseAction += new EventHandler(Event);
+                }
+                button3.BackColor = Color.Green;
+               
+
+            }
+ 
+        }
+
+        private void Event(object sender, EventArgs e) {
+
+            MouseHook.MSLLHOOKSTRUCT temp = (MouseHook.MSLLHOOKSTRUCT)sender;
+
+            switch (temp.MouseAction)
+            {
+                case "WM_LBUTTONDOWN":
+                    appendText("Mouse Event Detected : " + temp.MouseAction + "\t Position : " + temp.pt.x + " " + temp.pt.y);
+                    break;
+                case "WM_LBUTTONUP":
+                    appendText("Mouse Event Detected : " + temp.MouseAction + "\t Position : " + temp.pt.x + " " + temp.pt.y);
+                    break;
+                case "WM_RBUTTONUP":
+                    appendText("Mouse Event Detected : " + temp.MouseAction + "\t Position : " + temp.pt.x + " " + temp.pt.y);
+                    break;
+                case "WM_RBUTTONDOWN":
+                    appendText("Mouse Event Detected : " + temp.MouseAction + "\t Position : " + temp.pt.x + " " + temp.pt.y);
+                    break;
+                case "WM_MOUSEWHEEL":
+                    appendText("Mouse Event Detected : " + temp.MouseAction + "\t Position : " + temp.pt.x + " " + temp.pt.y);
+                    break;
+                case "WM_MOUSEMOVE":             
+                    break;
+                case "WM_MOUSEWHEELDOWN":
+                    appendText("Mouse Event Detected : " + temp.MouseAction + "\t Position : " + temp.pt.x + " " + temp.pt.y);
+                    break;
+                default:
+                    Console.WriteLine("Unknown mouse event!");
+                    break;
+            }
+ 
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            appendText2(" ------  All Running Programs ------");
+            // active programs
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                 
+                    appendText2("Name: " + p.MainWindowTitle + "\t\t StartTime: " + p.StartTime + "\t ProcessorTime: " + p.TotalProcessorTime + "\t VMsize: " + p.VirtualMemorySize + "\t PMsize: " + p.WorkingSet);
+                }
+            }
+
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // agents
+            // get list of Windows services
+            ServiceController[] services = ServiceController.GetServices();
+
+            appendText2(" ------  All Services ------");
+
+            // try to find service name
+            foreach (ServiceController service in services)
+            {
+                appendText2("Name: " + service.DisplayName + " Type: " + service.ServiceType + " Status: " +service.Status + " Depend: " + service.ServicesDependedOn);
+                
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // windows
+
+            if (windows)
+            {
+                UnhookWinEvent(hhook);
+                windows = false;
+                button5.BackColor = Color.Red;
+                appendText2("-----  Windows hook Ended ------");
+            }
+            else
+            {
+                windows = true;
+                appendText2("----- Starting Windows hook ------");
+                // Listen for name change changes across all processes/threads on current desktop...
+                hhook = SetWinEventHook(EVENT_OBJECT_NAMECHANGE, EVENT_OBJECT_NAMECHANGE, IntPtr.Zero,
+                        procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+
+                if (first2) {
+                    first2 = false;
+                WindowsAction += new EventHandler(updateWindowsText);
+                }
+
+                // MessageBox provides the necessary mesage loop that SetWinEventHook requires.
+                // In real-world code, use a regular message loop (GetMessage/TranslateMessage/
+                // DispatchMessage etc or equivalent.)
+
+                button5.BackColor = Color.Green;
+
+            }
+
+        }
+
+        private void updateWindowsText(object sender, EventArgs e)
+        {
+            appendText2(sender.ToString());
+            
+        }
+
+        public static event EventHandler WindowsAction = delegate { };
+
+        static void WinEventProc(IntPtr hWinEventHook, uint eventType,
+           IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+
+            WindowsAction("Event: " +eventType + " Thread: " + dwEventThread.ToString() + " HWND: " + hwnd.ToString() + " OBJECT: " + idObject.ToString() + " CHILD: " + idChild.ToString() + " TIME: " + dwmsEventTime.ToString(), new EventArgs());
+            
+        }
+
+    
+        private void appendText2(String s)
+        {
+            richTextBox2.AppendText(s +"\t" + "\n");
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.SelectionStart = richTextBox2.Text.Length;
+     
+            richTextBox2.ScrollToCaret();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // agents
+            // get list of Windows services
+            ServiceController[] services = ServiceController.GetServices();
+
+            appendText2(" ------  All None Stopped Services ------");
+
+            // try to find service name
+            foreach (ServiceController service in services)
+            {
+                if (service.Status != ServiceControllerStatus.Stopped) {
+                appendText2("Name: " + service.DisplayName + "\t\t Type: " + service.ServiceType + "\t Status: " + service.Status + "\t ServiceName: " + service.ServiceName);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+                if (programs)
+            {
+                MouseHook.stop();
+                programs = false;
+                button7.BackColor = Color.Red;
+            }
+            else
+            {
+                programs = true;
+                button7.BackColor = Color.Green;
+                MouseHook.Start();
+                if (first3)
+                {
+                    first3 = false;
+                    WatchPrograms();
+                }
+           
+
+
+            }
+        }
+
+        public void ProcessStartEventArrived(object sender, EventArrivedEventArgs e)
+        {
+            foreach (PropertyData pd in e.NewEvent.Properties)
+            {
+                //appendText2(pd.Name + " " + pd.Type + " " + pd.Value);
+                Console.WriteLine(pd.Name + " " + pd.Type + " " + pd.Value);
+                pd.
+            }
+        }
+
+        private void WatchPrograms()
+        {
+            ManagementEventWatcher w = new ManagementEventWatcher();
+            WqlEventQuery query = new WqlEventQuery("__InstanceCreationEvent",
+            new TimeSpan(0, 0, 1),
+            "TargetInstance isa \"Win32_Process\"");
+            try
+            {
+
+                w.Query = query;
+              //  Console.WriteLine(query.QueryString);
+                w.EventArrived += new EventArrivedEventHandler(this.ProcessStartEventArrived);
+                w.Start();
+                ManagementBaseObject l = w.WaitForNextEvent();
+                while (programs)
+                {
+                    w.WaitForNextEvent();
+                }
+
+            }
+            finally
+            {
+                w.Stop();
+            }
         }
     }
+
+
+
 }
+
